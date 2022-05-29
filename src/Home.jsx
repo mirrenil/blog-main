fb85c1f381391360f167de7a192cb58b0279997c
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2022-05-29 15:02:26 +0200

    fixed style of homepage with posts

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,46 +1,48 @@
-import BlogList from "./BlogList";
-import useFetch from "./useFetch";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getDocs, collection, deleteDoc, doc } from "firebase/firestore";
+import { db } from "./firebase";
 
-function Home() {
-  const { data: blogs, isPending } = useFetch("http://localhost:3000/blogs");
+function Home({ isAuth }) {
+  const [postList, setPostList] = useState([]);
+  const postCollectionRef = collection(db, "blogginlägg");
 
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postCollectionRef);
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getPosts();
+  }, []);
+
+  const deletePost = async (id) => {
+    const postDoc = doc(db, "blogginlägg", id);
+    await deleteDoc(postDoc);
+    console.log(postDoc);
+  };
   return (
-    <div className="home">
-      <h1>Senaste</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
-        accusantium quam similique tempore aliquam exercitationem illum placeat,
-        odit nobis totam nulla officiis, assumenda minima possimus aliquid,
-        suscipit quidem ea cupiditate?
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore sint
-        eveniet, quibusdam optio atque explicabo voluptatum fugiat? Repudiandae,
-        delectus qui quae sapiente, nesciunt aperiam corporis nam eligendi
-        laboriosam tempora repellendus?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
-        accusantium quam similique tempore aliquam exercitationem illum placeat,
-        odit nobis totam nulla officiis, assumenda minima possimus aliquid,
-        suscipit quidem ea cupiditate?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
-        accusantium quam similique tempore aliquam exercitationem illum placeat,
-        odit nobis totam nulla officiis, assumenda minima possimus aliquid,
-        suscipit quidem ea cupiditate?
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore sint
-        eveniet, quibusdam optio atque explicabo voluptatum fugiat? Repudiandae,
-        delectus qui quae sapiente, nesciunt aperiam corporis nam eligendi
-        laboriosam tempora repellendus?
-      </p>
-
-      {isPending && <div>Loading...</div>}
-      <BlogList blogs={blogs} title="Läs mer..." />
+    <div className="homePage">
+      {postList.map((post) => {
+        return (
+          <div className="post">
+            <div className="postHeader">
+              <div className="title">
+                <h3>{post.title}</h3>
+              </div>
+              <div className="delete-post">
+                {/* <button
+                  onClick={() => {
+                    deletePost(post.id);
+                  }}
+                >
+                  &#128465;
+                </button> */}
+              </div>
+            </div>
+            <div className="post-tex-container">{post.body}</div>
+          </div>
+        );
+      })}
     </div>
   );
 }
