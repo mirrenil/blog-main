c47bac5a98999cc99f0875bddbd5d4df2597d99b
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2022-05-30 11:52:21 +0200

    cleaned up header but added link to oter blogs

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,71 @@
 import React from "react";
-//import { useState, useEffect } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Outlet,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from "./Home";
-//import Create from "./Create";
-import BlogDetails from "./BlogDetails";
+import Create from "./Create";
+import BlogList from "./BlogList";
 import Profile from "./components/Profile";
-//import NotFound from "./NotFound";
 import Layout from "./Layout";
 import AuthProvider from "./contexts/AuthContext";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import ForgotPassword from "./components/ForgotPassword";
 import UpdateProfile from "./components/UpdateProfile";
-import { useAuth } from "./contexts/AuthContext";
 
 function App() {
   return (
     <Router>
+      <aside>
+        <nav
+          style={{
+            position: "absolute",
+            top: "12%",
+            left: "40%",
+          }}
+        >
+          <Link style={{ color: "#333" }} to="/bloglist">
+            Läs tidigare blogginlägg
+          </Link>
+        </nav>
+      </aside>
       <AuthProvider>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route path="/" element={<Public />} />
-            <Route index element={<Home />} />
-            <Route path="/blogs/:id" element={<BlogDetails />} />
-
-            <Route path="/private-outlet" element={<PrivateOutlet />}>
-              <Route path="" element={<Private />} />
-            </Route>
-            <Route
-              path="/private-nested"
-              element={
-                <PrivateRoute>
-                  <Private />
-                </PrivateRoute>
-              }
-            />
-            <Route path="/" element={<Profile />} />
-            <Route path="/update-password" element={<UpdateProfile />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
+            {/* {!isAuth ? ( */}
+            <>
+              <Route index element={<Home />} />
+              <Route path="/bloglist" element={<BlogList />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route
+                path="/login"
+                element={<Login /* setIsAuth={setIsAuth} */ />}
+              />
+            </>
+            {/*   ) : ( */}
+            <>
+              <Route index element={<Home />} />
+              <Route
+                path="/create"
+                element={<Create /* isAuth={isAuth} */ />}
+              />
+              <Route
+                path="/profile"
+                element={<Profile /* isAuth={isAuth} */ />}
+              />
+              <Route
+                path="/update-password"
+                element={<UpdateProfile /* isAuth={isAuth} */ />}
+              />
+              <Route
+                path="/forgot-password"
+                element={<ForgotPassword /* isAuth={isAuth} */ />}
+              />
+            </>
+            {/* )} */}
           </Route>
         </Routes>
       </AuthProvider>
     </Router>
   );
-}
-const Public = () => <div>public</div>;
-const Private = () => <div>private</div>;
-
-function PrivateOutlet() {
-  const auth = useAuth();
-  return auth ? <Outlet /> : <Navigate to="/login" />;
-}
-
-export function PrivateRoute({ children }) {
-  const { currentUser } = useAuth();
-  return currentUser ? children : <Navigate to="/login" />;
 }
 
 export default App;
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 //import logo from "./logo.png";
 //import BurgerMenu from "./BurgerMenu";
-import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "./contexts/AuthContext";
 
 const Header = () => {
   const navigate = useNavigate();
-  const { currentUser } = useAuth();
 
   const handleNavigate = () => {
     navigate("/");
@@ -20,21 +17,6 @@ const Header = () => {
           Madde & Kenta på vift
         </button>
       </div>
-
-      {/*  <BurgerMenu /> */}
-      {/* <Link to="/">Hem</Link>
-      <Link to="/blogs/:id">Tidigare bloggar</Link> */}
-
-      {/* <Container
-        className="d-flex align-items-center justify-content-center"
-        style={{ minHeight: "100vh" }}
-      >
-        <div className="w-100" style={{ maxWidth: "400px" }}>
-          <Signup />
-        </div>
-      </Container> */}
-      {/* OBS! endast synlig när inloggad */}
-      {/*  <Link to="/create">Skapa blogg </Link> */}
     </div>
   );
 };
