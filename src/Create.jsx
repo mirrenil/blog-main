44b0a945f9bc68bfd16a94260f1a15ac6ead9c4a
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2022-05-30 11:52:33 +0200

    trying to add images to post

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,24 +1,54 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router";
 import { addDoc, collection } from "firebase/firestore";
 import { db } from "./firebase";
+import { storage } from "./firebase";
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  list,
+  listAll,
+} from "firebase/storage";
 
 const Create = ({ isAuth }) => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
-  //const [image, setImage] = useState("");
+  const [image, setImage] = useState(null);
+  const [imageUrls, setImageUrls] = useState([]);
   const navigate = useNavigate("");
+
   const postCollectionRef = collection(db, "blogginlägg");
+  const imageListRef = ref(storage, "images/");
 
   const createPost = async () => {
     await addDoc(postCollectionRef, {
       title,
       body,
+      image,
       /* author: { name: auth.currentUser.displayName, id: auth.currentUser.uid } */
+    });
+    if (image == null) return;
+    const imageRef = ref(storage, `images/${image.name + Date.now()}`);
+    uploadBytes(imageRef, image).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        setImageUrls((prev) => [...prev, url]);
+      });
     });
     navigate("/");
   };
+
+  useEffect(() => {
+    listAll(imageListRef).then((response) => {
+      response.items.forEach((item) => {
+        getDownloadURL(item).then((url) => {
+          setImageUrls((prev) => [...prev, url]);
+        });
+      });
+    });
+  }, []);
 
   // useEffect(() => {
   //   if (!isAuth) {
@@ -28,9 +58,9 @@ const Create = ({ isAuth }) => {
 
   return (
     <div className="create">
-      <h2>Skriv ett nytt blogginlägg</h2>
       <label>Titel</label>
       <input
+        style={{ maxWidth: "300px" }}
         type="text"
         required
         value={title}
@@ -38,23 +68,28 @@ const Create = ({ isAuth }) => {
       />
       <label>Inlägg</label>
       <input
-        style={{ width: "400px", height: "200px" }}
+        style={{ maxWidth: "400px", minHeight: "300px" }}
         type="text"
         required
         value={body}
         onChange={(e) => setBody(e.target.value)}
       />
-      {/* <label>Lägg till en bild</label>
-        <input
-          type="url"
-          value={image}
-          onChange={(e) => setImage(e.target.value)}
-          name="image"
-          id="image"
-          placeholder="url här"
-        /> */}
-
-      <button onClick={createPost}>Publicera</button>
+      <label>Lägg till en bild</label>
+      <input
+        style={{ maWidth: "200px", border: "none" }}
+        type="file"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+        name="image"
+        id="image"
+      />
+      <Button
+        onClick={createPost}
+        className="btn btn-primary w-50 mt-3"
+        style={{ color: "white" }}
+      >
+        Publicera
+      </Button>
     </div>
   );
 };
