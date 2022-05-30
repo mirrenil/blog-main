b6a0641b6e7f86e6d8d1a430d47615f0007abbce
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2022-05-30 11:41:55 +0200

    updated the error boundary

--- src/ErrorBoundary.jsx
+++ src/ErrorBoundary.jsx
@@ -17,7 +17,7 @@ class ErrorBoundary extends React.Component {
   render() {
     if (this.state.hasError) {
       // You can render any custom fallback UI
-      return <h1>Something went wrong.</h1>;
+      return <h1>Cannot find the page you are searching for.</h1>;
     }
 
     return this.props.children;
