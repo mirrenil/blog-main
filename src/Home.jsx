52138305b706ebcd08f473a09dbbe72084bac56e
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2022-07-06 22:03:05 +0200

    trying to show the categories on the homepage

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import { db } from "./firebase";
+import {
+  getDocs,
+  doc,
+  orderBy,
+  query,
+  onSnapshot,
+  collection,
+} from "firebase/firestore";
 
 function Home(/* { isAuth } */) {
+  const [postList, setPostList] = useState([]);
+  const postCollectionRef = collection(db, "blogginlägg");
+
+  useEffect(() => {
+    const getPosts = async () => {
+      await getDocs(postCollectionRef);
+      const q = query(postCollectionRef, orderBy("categoryValue", "desc"));
+      onSnapshot(q, (snapshot) => {
+        const posts = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setPostList(posts);
+      });
+    };
+
+    getPosts();
+  }, []);
+
   return (
     <div>
-      <aside>
-        <nav></nav>
-      </aside>
+      <div className="categories">
+        {postList.map((post) => {
+          return (
+            <div className="category" key={post.id}>
+              {/* <div>{post.categoryValue}</div> */}
+            </div>
+          );
+        })}
+      </div>
       <BlogList />
     </div>
   );
