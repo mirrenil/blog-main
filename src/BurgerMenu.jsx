0e52fcea92d088554752b5d6f8d21129a0233012
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2022-07-18 11:51:17 +0200

    responsive burger menu is now working - need to add logic for the categories

--- src/BurgerMenu.jsx
+++ src/BurgerMenu.jsx
@@ -1,30 +1,102 @@
-import { Link } from "react-router-dom";
-import React from "react";
-import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { useAuth } from "./contexts/AuthContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { solid } from "@fortawesome/fontawesome-svg-core/import.macro"; // <-- import styles to be used
 
 const BurgerMenu = () => {
   const [open, setOpen] = useState(false);
+  const { currentUser } = useAuth();
+
+  const handleClick = () => setOpen(!open);
 
   return (
     <div>
       <nav className="navBar">
-        <button
-          style={{ border: "none", background: "transparent" }}
-          onClick={() => setOpen(!open)}
-        >
-          <FontAwesomeIcon icon={solid("bars")} style={{ fontSize: "2rem" }} />
-        </button>
+        <div className="nav-container">
+          <NavLink
+            to="/" // <-- link to home page
+            className="nav-icon"
+            onClick={handleClick}
+          >
+            <FontAwesomeIcon
+              icon={solid("bars")}
+              style={{ fontSize: "2rem" }}
+            />
+          </NavLink>
 
-        <ul className="">
-          {/* <li className="links">
-          <Link to="/">Hem</Link>
-        </li>
-        <li>
-          <Link to="/create">Skapa blogg</Link>
-        </li> */}
-        </ul>
+          <ul className={open ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <NavLink to="/" className="nav-links" activeclassname="active">
+                Hem
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/"
+                className="nav-links"
+                activeclassname="active"
+                onClick={handleClick}
+              >
+                Resor
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/"
+                className="nav-links"
+                activeclassname="active"
+                onClick={handleClick}
+              >
+                Familj
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/"
+                className="nav-links"
+                activeclassname="active"
+                onClick={handleClick}
+              >
+                Husbil
+              </NavLink>
+            </li>
+            {currentUser ? (
+              <>
+                <li className="nav-item">
+                  <NavLink
+                    to="/profile"
+                    className="nav-links"
+                    activeclassname="active"
+                    onClick={handleClick}
+                  >
+                    Profil
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  {/* <NavLink
+                    to="/create"
+                    className="nav-links"
+                    activeclassname="active"
+                  >
+                    Nytt blogginlägg
+                  </NavLink> */}
+                </li>
+              </>
+            ) : (
+              <li className="nav-item">
+                <NavLink
+                  to="/login"
+                  className="nav-links"
+                  activeclassname="active"
+                  onClick={handleClick}
+                >
+                  Logga in
+                </NavLink>
+              </li>
+            )}
+          </ul>
+        </div>
       </nav>
     </div>
   );
