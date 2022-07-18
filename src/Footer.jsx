a8235e98a9e925ca2b6bb466bb64393aba6cbeb9
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2022-07-18 11:51:31 +0200

    clean up

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "./contexts/AuthContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  solid,
-  regular,
-  brands,
-} from "@fortawesome/fontawesome-svg-core/import.macro"; // <-- import styles to be used
+import { solid } from "@fortawesome/fontawesome-svg-core/import.macro"; // <-- import styles to be used
 
 const Footer = () => {
   const { currentUser } = useAuth();
@@ -20,24 +16,8 @@ const Footer = () => {
             style={{ textDecoration: "none", color: "black" }}
             to="/profile"
           >
-            {/* <span role="img" aria-label="icon-profile">
-              👤
-            </span> */}
             <FontAwesomeIcon icon={solid("circle-user")} />
           </Link>
-          {/* <Link
-            className="new-blog"
-            style={{
-              textDecoration: "none",
-              color: "black",
-            }}
-            to="/create"
-          >
-            Nytt blogginlägg
-          </Link>
-          <button className="signout-btn" onClick={signUserOut}>
-            Logga ut
-          </button> */}
         </div>
       ) : (
         <Link
