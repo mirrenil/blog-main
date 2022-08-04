cdbb347e52b93689ab8ac4552d56d6b4823574bc
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2022-08-04 18:13:56 +0200

    removed unused code in home component which messed up the styling

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,45 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import BlogList from "./BlogList";
-import { db } from "./firebase";
-import {
-  getDocs,
-  orderBy,
-  query,
-  onSnapshot,
-  collection,
-} from "firebase/firestore";
 
 function Home(/* { isAuth } */) {
-  const [postList, setPostList] = useState([]);
-  const postCollectionRef = collection(db, "blogginlägg");
-
-  useEffect(() => {
-    const getPosts = async () => {
-      await getDocs(postCollectionRef);
-      const q = query(postCollectionRef, orderBy("categoryValue", "desc"));
-      onSnapshot(q, (snapshot) => {
-        const posts = snapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
-        setPostList(posts);
-      });
-    };
-
-    getPosts();
-  }, []);
-
   return (
     <div>
-      <div className="categories">
-        {postList.map((post) => {
-          return (
-            <div className="category" key={post.id}>
-              {/* <div>{post.categoryValue}</div> */}
-            </div>
-          );
-        })}
-      </div>
       <BlogList />
     </div>
   );
